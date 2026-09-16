0b67bf6edbfdff8e22d60a8dd5ab4d73b672f28d
G7volt <139160097+G7volt@users.noreply.github.com>
2026-09-16 08:09:19 -0600

    feat: configuracion del archivo env y el archivo babel

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,19 +2,24 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"; 
 import {getStorage} from "firebase/storage";
+import {API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID} from "@env"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0DbazAzJFo8lz5-KGmf1jS4thNaZSWAM",
-  authDomain: "evaluacionfirebase-b0dc6.firebaseapp.com",
-  projectId: "evaluacionfirebase-b0dc6",
-  storageBucket: "evaluacionfirebase-b0dc6.firebasestorage.app",
-  messagingSenderId: "821823770610",
-  appId: "1:821823770610:web:45d240da1cad5852e2656b"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const storage = getStorage(app);
+
+export {database, storage};
